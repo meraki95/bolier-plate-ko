5d7b020e70de33b5de8412ff4fe809ff813dd3d7
Your Name <you@example.com>
2024-02-19 20:59:00 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name : {
+        type:String,
+        maxlength: 50
+    },
+    email: {
+        type : String,
+        trim : true,
+        unique :1
+    },
+    password: {
+        type : String,
+        minlength :5
+    },
+
+    lastname: {
+        type : String,
+        maxlength: 50
+    },
+    role: {
+        type : Number, 
+        default:0      
+    },
+    image : String,
+    token : {
+        type : String
+    },
+    tokenExp:{
+        type: Number
+    }
+})
+
+const User = mongoose.model('User',userSchema)
+
+module.exports={}//다른곳에서도 쓸수있게
